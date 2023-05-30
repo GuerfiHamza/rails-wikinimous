ac295e5e07d09612a9aa083172782a06cc07434a
Guerfi Hamza <hmzaa600@gmail.com>
2023-05-30 13:07:40 +0100

    m

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Article.create(title: "Hello World", content: "This is my first article")
+Article.create(title: "Hello World 2", content: "This is my second article")
+Article.create(title: "Hello World 3", content: "This is my third article")
+Article.create(title: "Hello World 4", content: "This is my fourth article")
+Article.create(title: "Hello World 5", content: "This is my fifth article")
+Article.create(title: "Hello World 6", content: "This is my sixth article")
+Article.create(title: "Hello World 7", content: "This is my seventh article")
+Article.create(title: "Hello World 8", content: "This is my eighth article")
+Article.create(title: "Hello World 9", content: "This is my ninth article")
+Article.create(title: "Hello World 10", content: "This is my tenth article")
